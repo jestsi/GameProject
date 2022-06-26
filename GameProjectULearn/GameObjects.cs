d086c20b7600d8cfe70f24373c4cb04bf7072b0d
jestsi <gestse11@gmail.com>
2022-06-26 17:03:36 +0300

    Некоторые методы перенесены в GameObjects.cs

--- GameProjectULearn/GameObjects.cs
+++ GameProjectULearn/GameObjects.cs
@@ -9,251 +9,189 @@ public enum TypeObject
     Danger
 }
 /// <summary>
-/// Интерфейс представляющий объекты
+/// Абстракция представляющая объекты
 /// </summary>
-public interface IGameObjects
+public abstract class GameObjects
 {
-    public int X { get; set; }
-    public int Y { get; set; }
+    public virtual int X { get; protected set; }
+    public virtual int Y { get; protected set; }
 
-    public int Width { get; set; }
-    public int Height { get; set; }
-    public Color _Color { get; set; }
+    public virtual int Width { get; protected set; }
 
-    public TypeObject Type { get; }
+    public virtual int Height { get; protected set; }
+    public virtual Color Color { get; set; }
+
+    public virtual TypeObject Type { get; }
+
+    public GameObjects()
+    {
+        X = 0;
+        Y = 0;
+        Color = Color.Black;
+        Width = 1;
+        Height = 1;
+    }
+    
+    public GameObjects(int x, int y, int width, int height, Color color)
+    {
+        if (width < 0 || height < 0 || x < 0 || y < 0) 
+            throw new ArgumentException($"the argument under the name " +
+                                        $"{nameof(width)}," +
+                                        $"{nameof(height)}," +
+                                        $"{nameof(x)} or " +
+                                        $"{nameof(y)} is invalid");
+
+        X = x;
+        Y = y;
+        Color = color;
+        Width = width;
+        Height = height;
+
+        PositionChange = delegate(int x, int y, int newX, int newY) {};
+        ColorChanged = delegate(Color last, Color newColor) {}; 
+        SizeChanged = delegate(int width, int height) { };
+    }
+    
     /// <summary>
     /// Метод перемещает объект в двумерном пространстве
     /// </summary>
     /// <param name="x">Новая кордината объекта по x</param>
     /// <param name="y">Новая кордината объекта по y</param>
-    /// <param name="limiter">Проверять ли на выход из границ "ClientSize"</param>
-    /// <param name="clientSize">Нужно для проверки на выход из границ</param>
-    public void Move(int x, int y, bool limiter = true, Size? clientSize = null);
+    public virtual void Move(int x, int y)
+    {
+        if (x < 0 || y < 0 ) 
+            throw new ArgumentException($"the argument under the name {nameof(x)} or {nameof(y)} is invalid");
+        
+        PositionChange?.Invoke(X, Y, x, y);
+        X = x;
+        Y = y;
+    }
+
+    public virtual async void MoveAsync(int x, int y) => await Task.Run(() => Move(x, y));
+    public virtual async void ResizeAsync(int x, int y) => await Task.Run(() => Resize(x, y));
+    
+
     /// <summary>
     /// Метод изменяет размеры объекта
     /// </summary>
     /// <param name="width">Новая ширина объекта</param>
     /// <param name="height">Новая высота объекта</param>
-    public void Resize(int width, int height);
+    public virtual void Resize(int width, int height)
+    {
+        if (width <= 0 || height <= 0) 
+            throw new ArgumentException($"the argument under the name {nameof(width)} or {nameof(height)} is invalid");
+
+        SizeChanged?.Invoke(width, height);
+        Width = width;
+        Height = height;
+    }
     
     /// <summary>
     /// Событие при котором изменяеться позиция объекта
     /// </summary>
-    public event Action<int, int, int, int> PositionChange;
+    public virtual event Action<int, int, int, int> PositionChange;
     /// <summary>
     /// Событие при котором изменяеться цвет объекта
     /// </summary>
-    public event Action<Color, Color> ColorChanged;
+    public virtual event Action<Color, Color> ColorChanged;
     /// <summary>
     /// Событие при котором изменяеться размер объекта
     /// </summary>
-    public event Action<int, int> SizeChanged;
+    public virtual event Action<int, int> SizeChanged;
 }
 
 public class BuildingStructs
 {
-    public class Wall : IGameObjects
+    public sealed class Wall : GameObjects
     {
-        private int _x;
-        private int _y;
-        private int _width;
-        private int _height;
-        private Color _color; 
-        // private bool _isFake; 
+        private Color _color;
         private const TypeObject _Type = TypeObject.Building;
+        public override event Action<int, int, int, int> PositionChange;
+        public override event Action<Color, Color> ColorChanged;
+        public override event Action<int, int> SizeChanged;
 
-        public Wall(int x, int y, int width, int height, Color color)
-        {
-            if (width < 0 || height < 0 || x < 0 || y < 0) 
-                throw new ArgumentException($"the argument under the name " +
-                                            $"{nameof(width)}," +
-                                            $"{nameof(height)}," +
-                                            $"{nameof(x)} or " +
-                                            $"{nameof(y)} is invalid");
-
-            this._x = x;
-            this._y = y;
-            this._color = color;
-            this._width = width;
-            this._height = height;
-            //_isFake = false;
-            
-            PositionChange = delegate(int x, int y, int newX, int newY) {};
-            ColorChanged = delegate(Color last, Color newColor) {}; 
-            SizeChanged = delegate(int width, int height) { };
-        }
-
-        public void Move(int x, int y, bool limiter = true, Size? clientSize = null)
-        {
-            if (x < 0 || y < 0 ) 
-                throw new ArgumentException($"the argument under the name {nameof(x)} or {nameof(y)} is invalid");
-            
-            PositionChange?.Invoke(_x, _y, x, y);
-            _x = x;
-            _y = y;
-        }
-
-        public async void MoveAsync(int x, int y)
-        {
-            await Task.Run(() => Move(x, y));
-        }
-
-        public void Resize(int width, int height)
-        {
-            if (width <= 0 || height <= 0) 
-                throw new ArgumentException($"the argument under the name {nameof(width)} or {nameof(height)} is invalid");
-            SizeChanged?.Invoke(width, height);
-            _width = width;
-            _height = height;
-        }
-
-        public async void ResizeAsync(int width, int height)
-        {
-            await Task.Run(() => Resize(width, height));
-        }
+        public Wall(int x, int y, int width, int height, Color color) 
+            : base(x, y, width, height, color)
+        { }
         
-        public event Action<int, int, int, int> PositionChange;
-        public event Action<Color, Color> ColorChanged;
-        public event Action<int, int> SizeChanged; 
-
-        public int X
-        {
-            get => _x;
-            set => _x = value;
-        }
-
-        public int Y
-        {
-            get => _y;
-            set => _y = value;
-        }
-
-        public int Width
-        {
-            get => _width;
-            set => _width = value;
-        }
-
-        public int Height
-        {
-            get => _height;
-            set => _height = value;
-        }
-
         [DefaultValue(typeof(Color), "Color.Black")]
-        public Color _Color
+        public override Color Color
         {
             get => _color;
             set
             {   
-                ColorChanged?.Invoke(_Color, value);
+                ColorChanged?.Invoke(Color, value);
                 _color = value;
             }
         }
-
-        public TypeObject Type
-        {
-            get => _Type;
-        }
+        public override TypeObject Type => _Type;
     }
 
-    public class Platform : IGameObjects
+    public sealed class Platform : GameObjects
     {
-        private int _x;
-        private int _y;
-        private int _width;
-        private int _height;
         private Color _color;
         private const TypeObject _Type = TypeObject.Building;
 
-        public Platform(int x, int y, int width, int height, Color color)
+        public Platform(int x, int y, int width, int height, Color color) : base(x, y, width, height, color)
         {
-            if (width < 0 || height < 0 || x < 0 || y < 0) 
-                throw new ArgumentException($"the argument under the name " +
-                                            $"{nameof(width)}," +
-                                            $"{nameof(height)}," +
-                                            $"{nameof(x)} or " +
-                                            $"{nameof(y)} is invalid");
-            _x = x;
-            _y = y;
-            _width = width;
-            _height = height;
-            _color = color;
-
-            PositionChange = delegate(int x, int y, int newX, int newY) {};
-            ColorChanged = delegate(Color last, Color newColor) {}; 
-            SizeChanged = delegate(int width, int height) { };
+            
         }
-
+        
         public void Move(int x, int y, bool limiter = true, Size? clientSize = null)
         {
             if (x < 0 || y < 0 || (limiter && clientSize == null)) 
                 throw new ArgumentException($"the argument under the name {nameof(x)} or {nameof(y)} is invalid");
 
-            if (limiter && (x+_width) <= clientSize.Value.Width && 
+            if (limiter && (x+Width) <= clientSize.Value.Width && 
                 x >= 0 && y >= 0)
             {
-                PositionChange?.Invoke(_x, _y, x, y);
-                _x = x;
-                _y = y;
+                PositionChange?.Invoke(X, Y, x, y);
+                X = x;
+                Y = y;
                 return;
             }
 
             if (limiter) return;
             
-            PositionChange?.Invoke(_x, _y, x, y);
-            _x = x;
-            _y = y;
+            PositionChange?.Invoke(X, Y, x, y);
+            X = x;
+            Y = y;
         }
         
         public async void MoveAsync(int x, int y, bool limiter = true, Size? clientSize = null)
         {
             await Task.Run(() => Move(x, y, limiter, clientSize));
         }
-        
-        public void Resize(int width, int height)
-        {
-            if (width <= 0 || height <= 0) 
-                throw new ArgumentException($"the argument under the name {nameof(width)} or {nameof(height)} is invalid");
-            SizeChanged?.Invoke(width, height);
-            _width = width;
-            _height = height;
-        }
-        
-        public async void ResizeAsync(int width, int height)
-        {
-            await Task.Run(() => Resize(width, height));
-        }
-        
-        public event Action<int, int, int, int> PositionChange;
-        public event Action<Color, Color> ColorChanged;
-        public event Action<int, int> SizeChanged; 
 
-        public int X
+        public static void MovePlatform(in KeyEventArgs args, in Platform platform, Size clientSize, int stepSize = 10)
         {
-            get => _x;
-            set => _x = value;
+            if (args == null) 
+                throw new ArgumentNullException(nameof(args));
+        
+            int newFloorX;
+
+            if (args.KeyCode is Keys.A or Keys.Left)
+                newFloorX = -stepSize + platform.X;
+            else
+                newFloorX = stepSize + platform.X;
+        
+            if (newFloorX < 0 ) return;
+        
+            platform.Move(newFloorX, platform.Y, true, clientSize);
         }
         
-        public int Y
+        public async void MovePlatformAsync(object? sender, KeyEventArgs args, Size clientSize)
         {
-            get => _y;
-            set => _y = value;
+            if (args.KeyCode is not (Keys.A or Keys.Left or Keys.D or Keys.Right))
+                return;
+            await Task.Run(async () => MovePlatform(args, this, clientSize));
         }
         
-        public int Width
-        {
-            get => _width;
-            set => _width = value;
-        }
-        
-        public int Height
-        {
-            get => _height;
-            set => _height = value;
-        }
-        
-        public Color _Color
+        public override event Action<int, int, int, int>? PositionChange;
+        public override event Action<Color, Color>? ColorChanged;
+        public override event Action<int, int>? SizeChanged;
+
+        public override Color Color
         {
             get => _color;
             set
@@ -263,11 +201,7 @@ public class BuildingStructs
             }
         }
 
-        public TypeObject Type
-            {
-                get => _Type;
-            }
-            
+        public override TypeObject Type => _Type;
     }
     
 }
--- GameProjectULearn/MainField.cs
+++ GameProjectULearn/MainField.cs
@@ -1,78 +1,86 @@
 namespace GameProjectULearn;
 
-public class MainField : Form
+public sealed class MainField : Form
 {
-    private readonly BuildingStructs.Platform _platform;
-    private readonly BuildingStructs.Platform _platform_p2;
-    private readonly BuildingStructs.Wall leftWall;
-    private readonly BuildingStructs.Wall rightWall;
-
     public MainField()
     {
         ClientSize = new Size(300, 300);
         DoubleBuffered = true; // двойная буфферизация для того что бы исчесзло мерцание 
-        
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
         const int wallsWidth = 5; 
 
-        _platform = new ((ClientSize.Width / 4) + 10, ClientSize.Height - 20, ClientSize.Width/2, ClientSize.Height, Color.Brown);
-        _platform_p2 = new(ClientSize.Width / 4 + 10, 0, ClientSize.Width / 2, 20, Color.Blue);
+        var platform = new BuildingStructs.Platform((ClientSize.Width / 4) + 10, ClientSize.Height - 20, ClientSize.Width/2, 20, Color.Brown);
+        var platformP2 = new BuildingStructs.Platform(ClientSize.Width / 4 + 10, 0, ClientSize.Width / 2, 20, Color.Blue);
         
-        leftWall = new(0, 0, wallsWidth, ClientSize.Height, Color.Crimson);
-        rightWall = new(ClientSize.Width, 0, wallsWidth, ClientSize.Height, Color.Crimson);
+        BuildingStructs.Wall leftWall = new(0, 0, wallsWidth, ClientSize.Height, Color.Crimson);
+        BuildingStructs.Wall rightWall = new(ClientSize.Width, 0, wallsWidth, ClientSize.Height, Color.Crimson);
         
         // нижние четыре строки нужны что бы при изменении позции объекта обновлялся холст(поле)
-        rightWall.PositionChange += (x, y, newX, newY) => Invalidate();
-        leftWall.PositionChange += (x, y, newX, newY) => Invalidate();
-        _platform.PositionChange += (x, y, newX, newY) => Invalidate();
-        _platform_p2.PositionChange += (x, y, newX, newY) => Invalidate();
+        rightWall.PositionChange += async (_, _, _, _) => Invalidate();
+        leftWall.PositionChange += async (_, _, _, _) => Invalidate();
+        platform.PositionChange += async (_, _, _, _) => Invalidate();
+        platformP2.PositionChange += async (_, _, _, _) => Invalidate();
 
-        ClientSizeChanged += (obj, args) =>
+        ClientSizeChanged += async (_, _) =>
         {
-            _platform.MoveAsync((ClientSize.Width / 4) + 10, 
+            platform.MoveAsync((ClientSize.Width / 4) + 10, 
                 ClientSize.Height - 20, true, ClientSize);
-            _platform.ResizeAsync(ClientSize.Width/2, ClientSize.Height);
+            platform.ResizeAsync(ClientSize.Width/2, platform.Height);
         };
 
-        ClientSizeChanged += (obj, args) =>
+        ClientSizeChanged += async (_, _) =>
         {
-            _platform_p2.MoveAsync((ClientSize.Width / 4) + 10, 
+            platformP2.MoveAsync((ClientSize.Width / 4) + 10, 
                 0, true, ClientSize);
-            _platform.ResizeAsync(ClientSize.Width/2, ClientSize.Height);
+            platformP2.ResizeAsync(ClientSize.Width/2, platformP2.Height);
         };
 
-        ClientSizeChanged += (obj, args) =>
+        ClientSizeChanged += async (_, _) =>
         {
             rightWall.MoveAsync(ClientSize.Width, 0);
             rightWall.ResizeAsync(wallsWidth, ClientSize.Height);
         };
         
-        ClientSizeChanged += (obj, args) =>
+        ClientSizeChanged += async (_, _) =>
         {
             leftWall.MoveAsync(0, 0);
             leftWall.ResizeAsync(wallsWidth, ClientSize.Height);
         };
 
-        Paint += (sender, args) =>
+        KeyDown += async (_, args) =>
+        {
+            if (args.KeyCode is Keys.A or Keys.D)
+                platformP2.MovePlatformAsync(this, args, ClientSize);
+            else if (args.KeyCode is Keys.Left or Keys.Right)
+                platform.MovePlatformAsync(this, args, ClientSize);
+        };
+        
+        Paint += async (_, args) =>
         {   
             args.Graphics.ResetTransform();
 
             Draw(args, leftWall);
             Draw(args, rightWall);
 
-            Draw(args, _platform);
-            Draw(args, _platform_p2);
+            Draw(args, platform);
+            Draw(args, platformP2);
 
         };
+        base.OnLoad(e);
     }
-    private static void Draw(PaintEventArgs p, IGameObjects obj)
+
+    private static void Draw(PaintEventArgs p, GameObjects obj)
     {
         switch (obj.GetType().Name)
         {
             case "Platform" when obj is BuildingStructs.Platform pl:
-                p.Graphics.FillRectangle(new SolidBrush(pl._Color),pl.X, pl.Y, pl.Width, pl.Height);
+                p.Graphics.FillRectangle(new SolidBrush(pl.Color),pl.X, pl.Y, pl.Width, pl.Height);
                 break;
             case "Wall" when obj is BuildingStructs.Wall wl:
-                p.Graphics.DrawLine(new Pen(wl._Color, wl.Width), 
+                p.Graphics.DrawLine(new Pen(wl.Color, wl.Width), 
                     wl.X, wl.Y, 
                     wl.X, wl.Height);
                 break;
@@ -81,29 +89,6 @@ public class MainField : Form
 
         }
     }
+
     
-    private void MovePlatform(KeyEventArgs e, BuildingStructs.Platform pl, int stepSize = 10)
-    {
-        if (e == null) throw new ArgumentNullException(nameof(e));
-        
-        var newFloorX = 0;
-
-        if (e.KeyCode is Keys.A or Keys.Left)
-            newFloorX = -stepSize + pl.X;
-        else
-            newFloorX = stepSize + pl.X;
-        
-        if (newFloorX < 0 ) return;
-        
-        pl.MoveAsync(newFloorX, pl.Y, true, ClientSize);
-    }
-
-    protected override void OnKeyDown(KeyEventArgs e)
-    {
-        if (e.KeyCode is Keys.Left or Keys.Right)
-            MovePlatform(e, _platform);
-        else if (e.KeyCode is Keys.A or Keys.D)
-            MovePlatform(e, _platform_p2);
-        
-    }
 }
